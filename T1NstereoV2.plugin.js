4acbc2f490563c228898be19052bc884d1a01c99
tinguy1 <130340201+tinguy1@users.noreply.github.com>
2023-06-10 21:08:25 -0500

    Update T1NstereoV2.plugin.js

--- T1NstereoV2.plugin.js
+++ T1NstereoV2.plugin.js
@@ -14,7 +14,7 @@ $$$$$$$$\   $$\   $$\   $$\        $$$$$$\ $$$$$$$$\ $$$$$$$$\ $$$$$$$\  $$$$$$$
 
  * @name T1NstereoV2
  * @author tinguy1
- * @version 1.0.6
+ * @version 1.0.7
  * @authorLink https://github.com/tinguy1
  * @invite 9bpbS4kjdf
  * @source https://github.com/tinguy1/T1NstereoV2
@@ -93,7 +93,7 @@ module.exports = (() => {
   const config = {
     info: {
       name: 'T1NstereoV2', //dont try to change the name of the plugin or it wont work
-      version: '1.0.6',
+      version: '1.0.7',
       description:
         '(You need a valid license to use this plugin) Disable echo cancellation, noise reduction, noise suppression, Diagnostic audio recording, and Debug logging all located in voice and video settings for this plugin to work, open plugin settings to see configurable settings. If you require support click on the question mark under the plugin name to join the support server.',
       authors: [
@@ -109,7 +109,7 @@ module.exports = (() => {
       {
         title: 'V2 release and bug fixes',
         items: [
-          'fixed problem where auto rejoin disapears when server muted'
+          'fixed problem where auto rejoin disapears when server muted',
           'fixed auto rejoin',
           'fixed auto rejoin again ugh, sorry for the plugin update prompts',
           'fixed auto rejoin againnnnnnnnnnnnnnnnnnn',
